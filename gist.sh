f42d626feefa81ca299262947b0d47cab19c0625
crccheck <c@crccheck.com>
2012-09-30 05:39:15 +0000

    update gist uploader to only upload gists that are dirty

--- gist.sh
+++ gist.sh
@@ -5,17 +5,31 @@ FOOTER="./docs-src/footer.html"
 
 EXAMPLE=$1
 
-# takes input
-# echo test=$test
-# echo $content
+# return 0 if file $1 was recently changed and should be pushed
+# TODO if file was changed in HEAD, push it
+function changed {
+    if [[ -z $(git status $1 -s --porcelain) ]]; then
+        return 1
+    fi
+    return 0
+}
+
 IS_GIST=$(cat $EXAMPLE | head -1 | grep gist)
-# echo $EXAMPLE $IS_GIST
+
 if [ -n "$IS_GIST" ]; then
     echo "Uploading $EXAMPLE"
     GIST=${IS_GIST##*/}
-    cat $HEADER $EXAMPLE $FOOTER | jist -f index.html -u $GIST
+    changed $EXAMPLE
+    if [ $? -eq 0 ]; then
+        echo "upload $EXAMPLE"
+        cat $HEADER $EXAMPLE $FOOTER | jist -f index.html -u $GIST
+    fi
     README=${EXAMPLE/%js/md}
     if [ -f $README ]; then
-        cat $README | jist -f README.md -u $GIST
+        changed $README
+        if [ $? -eq 0 ]; then
+            echo "upload $README"
+            cat $README | jist -f README.md -u $GIST
+        fi
     fi
 fi
